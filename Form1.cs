bc5c31b6e39f5e19deda005c526cbe4483e28098
eminnq <eminnq216@gmail.com>
2024-09-25 20:47:36 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,65 @@
+namespace hesapmakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            int bölme = sayi1 / sayi2;
+            label4.Text = bölme.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            int cikarma = sayi1 - sayi2;
+            label4.Text = cikarma.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            int toplama = sayi1 + sayi2;
+            label4.Text = toplama.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            int carpma = sayi1 * sayi2;
+            label4.Text = carpma.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            int modalma = sayi1 % sayi2;
+            label4.Text = modalma.ToString();
+        }
+    }
+}
